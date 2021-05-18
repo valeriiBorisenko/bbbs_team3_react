d8da824b2f236dbb83aab2d510e8225f1d6bd016
Alyona Irisova <elaineirr.910@gmail.com>
2021-05-19 00:01:31 +0400

    feat: добавить компоненты MainTitle и Rubric

---
+++ src/components/ui/MainTitle.js
@@ -0,0 +1,14 @@
+import './MainTitle.scss';
+import PropTypes from 'prop-types';
+
+function MainTitle({ title }) {
+  return (
+    <h1 className="main-title">{title}</h1>
+  );
+}
+
+MainTitle.propTypes = {
+  title: PropTypes.string,
+};
+
+export default MainTitle;
