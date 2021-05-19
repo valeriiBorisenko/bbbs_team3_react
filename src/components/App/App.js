e157bd7e2d332f75df234f42af2fc611fa9a8e20
Alyona Irisova <elaineirr.910@gmail.com>
2021-05-19 20:41:25 +0400

    refactor: изменить структуру файлов

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import Header from './ui/Header';
+import './App.scss';
+import Header from '../ui/Header';
 
 function App() {
   return (
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import { BrowserRouter } from 'react-router-dom';
-import App from './components/App';
+import App from './components/App/App';
 
 ReactDOM.render(
   <React.StrictMode>
