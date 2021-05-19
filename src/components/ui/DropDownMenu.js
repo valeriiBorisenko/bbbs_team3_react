c94ea7543205fddef572a0450569f49511fca37f
Nikita Volkhonskiy <literature198@gmail.com>
2021-05-19 18:06:30 +0300

    feat: добавить компонент DropDownMenu и его стили

---
+++ src/components/ui/DropDownMenu.js
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types';
+// import { NavLink } from 'react-router-dom';
+import NavItem from './NavItem';
+
+function DropDownMenu({ wrapperClasses, textsForLinks }) {
+  return (
+    <ul className={wrapperClasses}>
+      {textsForLinks.map((text) => (
+        <NavItem
+          wrapperClasses="menu__dropdown-list-item"
+          linkClasses="link menu__dropdown-link"
+          href="#"
+          text={text}
+        />
+      ))}
+    </ul>
+  );
+}
+
+DropDownMenu.propTypes = {
+  wrapperClasses: PropTypes.string.isRequired,
+  // eslint-disable-next-line react/forbid-prop-types
+  textsForLinks: PropTypes.array.isRequired
+};
+
+export default DropDownMenu;
