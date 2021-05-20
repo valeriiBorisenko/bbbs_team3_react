ef0a0183693384b4b140bb8cbb53b49f3d53b0d0
Alyona Irisova <elaineirr.910@gmail.com>
2021-05-20 14:18:42 +0400

    feat: добавить компонент CardAnnotation

---
+++ src/components/ui/CardAnnotation/CardAnnotation.js
@@ -0,0 +1,28 @@
+import './CardAnnotation.scss';
+import PropTypes from 'prop-types';
+
+function CardAnnotation({ isChosen, info, description }) {
+  return (
+    <div className={`card-annotation ${isChosen ? 'card-annotation_main' : ''}`}>
+      <div className="card-annotation__content">
+        {info && <p className="caption">{info}</p>}
+        <div className="card-annotation__desc card-annotation__desc_main">
+          <p className="paragraph card-annotation__paragraph">{description}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+CardAnnotation.propTypes = {
+  info: PropTypes.string,
+  description: PropTypes.string.isRequired,
+  isChosen: PropTypes.bool
+};
+
+CardAnnotation.defaultProps = {
+  info: undefined,
+  isChosen: false
+};
+
+export default CardAnnotation;
