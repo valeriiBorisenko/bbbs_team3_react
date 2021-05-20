4e08748f09821c0cd1f74c3ce5a1e9bc0dd4ac24
Nikita Volkhonskiy <literature198@gmail.com>
2021-05-20 12:41:46 +0300

    feat: добавить компонент Footer и подключить его в App компонент

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import './Footer.scss';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <a href="./index.html" className="footer__logo" target="_self">
+        <img className="footer__logo-image" src="./images/svg/footer-logo.svg" alt="Логотип Старшие Братья Старшие Сестры России" />
+      </a>
+    </footer>
+  );
+}
+
+export default Footer;
