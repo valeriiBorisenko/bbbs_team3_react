d8857988f5719414f48d9a9374e9476124e2eb82
Alyona Irisova <elaineirr.910@gmail.com>
2021-05-22 19:53:48 +0400

    refactor: рефакторинг некоторых Ui-компонентов, добавить UI_README

--- src/components/ui/SearchButton/SearchButton.js
+++ src/components/ui/SearchButton/SearchButton.js
@@ -1,14 +1,24 @@
 import './SearchButton.scss';
+import PropTypes from 'prop-types';
 
-function SearchButton() {
+function SearchButton({ handleClick }) {
   return (
     <button
       className="search-button search__button"
       type="submit"
       aria-label="Поиск"
       title="Поиск"
+      onClick={handleClick}
     />
   );
 }
+
+SearchButton.propTypes = {
+  handleClick: PropTypes.func
+};
+
+SearchButton.defaultProps = {
+  handleClick: undefined
+};
 
 export default SearchButton;
