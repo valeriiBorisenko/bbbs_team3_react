5526e49556b9d657aad8a5fcaff5b73ee0b4ee44
Alyona Irisova <elaineirr.910@gmail.com>
2021-05-30 16:23:25 +0400

    refactor: change props for AccountEventCard

--- src/components/Account/Account.js
+++ src/components/Account/Account.js
@@ -1,7 +1,6 @@
 import { useEffect, useState } from 'react';
 import './Account.scss';
 import { getAccountData } from '../../utils/api';
-import { formatDate } from '../../utils/utils';
 import Loader from '../ui/Loader/Loader';
 import AccountEventCard from '../ui/AccountEventCard/AccountEventCard';
 
@@ -13,6 +12,7 @@ function Account() {
       .then((res) => setEvents(res.data.accountData))
       .catch((err) => console.log(err));
   }, []);
+
   return events ? (
     <section className="account fade-in">
       <div className="account__events-area page__section">
@@ -26,9 +26,7 @@ function Account() {
             && events.map((item) => (
               <AccountEventCard
                 key={item.id}
-                day={formatDate(item.startAt).day}
-                month={formatDate(item.startAt).month}
-                title={item.title}
+                data={item}
               />
             ))}
         </div>
--- src/components/ui/AccountEventCard/AccountEventCard.js
+++ src/components/ui/AccountEventCard/AccountEventCard.js
@@ -1,29 +1,24 @@
 import './AccountEventCard.scss';
 import PropTypes from 'prop-types';
+import { formatDate } from '../../../utils/utils';
 import Card from '../Card/Card';
 
-function AccountEventCard({ day, month, title }) {
+function AccountEventCard({ data }) {
+  const startDay = formatDate(data.startAt);
+
   return (
     <Card sectionClass="account-event-card">
       <div className="account-event-card__date">
-        <span className="account-event-card__day">{day}</span>
-        <span className="account-event-card__month">{month}</span>
+        <span className="account-event-card__day">{startDay.day}</span>
+        <span className="account-event-card__month">{startDay.month}</span>
       </div>
-      <p className="account-event-card__title">{title}</p>
+      <p className="account-event-card__title">{data.title}</p>
     </Card>
   );
 }
 
 AccountEventCard.propTypes = {
-  day: PropTypes.string,
-  month: PropTypes.string,
-  title: PropTypes.string
-};
-
-AccountEventCard.defaultProps = {
-  day: '',
-  month: '',
-  title: ''
+  data: PropTypes.objectOf(PropTypes.string).isRequired
 };
 
 export default AccountEventCard;
