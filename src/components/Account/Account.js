c14da86c38db376187c4f6e85b297be6f0d650ad
Alyona Irisova <elaineirr.910@gmail.com>
2021-06-01 13:56:53 +0400

    feat: обновить страницу Account

--- src/components/Account/Account.js
+++ src/components/Account/Account.js
@@ -1,9 +1,11 @@
 import { useEffect, useState } from 'react';
 import './Account.scss';
 import { getAccountData } from '../../utils/api';
-import Loader from '../ui/Loader/Loader';
 import AccountEventCard from '../ui/AccountEventCard/AccountEventCard';
 import ScrollableByXContainer from '../ui/ScrollableByXContainer/ScrollableByXContainer';
+import TitleH2 from '../ui/TitleH2/TitleH2';
+import Loader from '../ui/Loader/Loader';
+import AccountForm from '../ui/AccountForm/AccountForm';
 
 function Account() {
   const [events, setEvents] = useState(null);
@@ -14,24 +16,35 @@ function Account() {
       .catch((err) => console.log(err));
   }, []);
 
-  return events ? (
+  return (
     <section className="account fade-in">
       <div className="account__events-area page__section">
-        <h2 className="section-title account__title">
-          {events.length > 0
-            ? 'Вы записаны на мероприятия:'
-            : 'У вас нет записи на мероприятия'}
-        </h2>
-        <ScrollableByXContainer sectionClass="account__events">
-          {events.length > 0
+        {!events ? <Loader /> : (
+          <>
+            <TitleH2
+              sectionClass="account__title"
+              title={events.length > 0 ? 'Вы записаны на мероприятия:' : 'У вас нет записи на мероприятия'}
+            />
+            <ScrollableByXContainer sectionClass="account__events">
+              {events.length > 0
             && events.map((item) => (
               <AccountEventCard key={item.id} data={item} />
             ))}
-        </ScrollableByXContainer>
+            </ScrollableByXContainer>
+          </>
+        )}
+      </div>
+
+      <div className="account__stories page__section">
+        <TitleH2
+          sectionClass="account__title"
+          title="Составьте историю вашей дружбы с младшим. Эта страница доступна только вам."
+        />
+        <div className="account__stories-container">
+          <AccountForm />
+        </div>
       </div>
     </section>
-  ) : (
-    <Loader />
   );
 }
 
