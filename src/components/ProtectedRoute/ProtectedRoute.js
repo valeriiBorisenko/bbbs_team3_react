6975ef8974ea0f6d130efc32a60d3e74be0b1f5d
Nikita Volkhonskiy <literature198@gmail.com>
2021-06-02 19:54:41 +0300

    feat: добавить компонент ProtectedRoute в проект

---
+++ src/components/ProtectedRoute/ProtectedRoute.js
@@ -0,0 +1,23 @@
+/* eslint-disable react/jsx-props-no-spreading */
+//! не дает использовать spread на пропсах, в дальнейшем заменить!
+// import { useContext } from 'react';
+// import CurrentUserContext from '../../contexts/CurrentUserContext';
+// eslint-disable-next-line react/jsx-props-no-spreading
+import PropTypes from 'prop-types';
+import { Redirect, Route } from 'react-router-dom';
+
+function ProtectedRoute({ component: Component, ...props }) {
+  return (
+    <Route>
+      {() => (props.isAuthorized ? <Component {...props} /> : <Redirect to={props.path} />) }
+    </Route>
+  );
+}
+
+ProtectedRoute.propTypes = {
+  component: PropTypes.node.isRequired,
+  isAuthorized: PropTypes.bool.isRequired,
+  path: PropTypes.string.isRequired
+};
+
+export default ProtectedRoute;
