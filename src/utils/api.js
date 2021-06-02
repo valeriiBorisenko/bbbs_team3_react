a1da1cd0378cfdd4bc8f5b67f81b27c8215354bd
Nikita Volkhonskiy <literature198@gmail.com>
2021-06-02 12:37:41 +0300

    feat: добавить функции и дату для работы с токеном в api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,18 +1,18 @@
 const axios = require('axios');
 // eslint-disable-next-line import/no-extraneous-dependencies
 const MockAdapter = require('axios-mock-adapter');
-
+//! data for server-responses
 const mainPageData = require('./server-responses/main-page.json');
-
 const accountData = require('./server-responses/account.json');
 const cities = require('./server-responses/cities.json');
-
 const calendarPageData = require('./server-responses/calendar-page.json');
+const token = require('./server-responses/token.json');
 
 const baseURL = 'http://localhost:3000';
 
 // mock
 const mock = new MockAdapter(axios, { delayResponse: 3000 });
+//! templated server responses
 // arguments for reply are (status, data, headers)
 mock.onGet(`${baseURL}/main/`).reply(
   200,
@@ -46,8 +46,17 @@ mock.onGet(`${baseURL}/afisha/events/`).reply(
   'Content-Type: application/json'
 );
 
-// functions
+mock.onGet(`${baseURL}/sign-in/`).reply(
+  200,
+  {
+    token
+  },
+  'Content-Type: application/json'
+);
+
+//! functions (new fetch-es)
 export const getMainPageData = () => axios.get(`${baseURL}/main/`);
 export const getAccountData = () => axios.get(`${baseURL}/account/`);
 export const getCities = () => axios.get(`${baseURL}/cities/`);
 export const getCalendarPageData = () => axios.get(`${baseURL}/afisha/events/`);
+export const getTokenOnLogin = () => axios.get(`${baseURL}/sign-in/`);
