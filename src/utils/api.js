2b6408cc3f97ebae00fb0dda1b098eee2ec4c885
Nikita Volkhonskiy <literature198@gmail.com>
2021-06-05 17:55:18 +0300

    feat: переработано Api, сделано на классе

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,119 +1,58 @@
-const axios = require('axios');
-// eslint-disable-next-line import/no-extraneous-dependencies
-const MockAdapter = require('axios-mock-adapter');
-
-//! data for server-responses
-//! надо изменить нейминг
-//! изменение нейминга тут влечет изменение на всех страницах сайта, где доступ к этим данным
-const mainPageData = require('./server-responses/main-page.json');
-const profileDiaryData = require('./server-responses/profile-diary.json');
-const cities = require('./server-responses/cities.json');
-const calendarPageData = require('./server-responses/calendar-page.json');
-const token = require('./server-responses/token.json');
-const userData = require('./server-responses/userData.json');
+import axios from 'axios';
+import setMockedAnswers from './mocked-answers';
 
 const baseURL = 'http://localhost:3000';
 const apiUrl = '/api/v1';
 
-// mock
-const mock = new MockAdapter(axios, { delayResponse: 1000 });
-//! templated server responses
-// arguments for reply are (status, data, headers)
-//! если отправлять просто как mainPageData без {} будет ошибка
-mock.onGet(`${baseURL}${apiUrl}/main/`).reply(
-  200,
-  {
-    mainPageData
-  },
-  'Content-Type: application/json'
-);
+setMockedAnswers();
 
-mock.onGet(`${baseURL}${apiUrl}/profile/diary/`).reply(
-  200,
-  {
-    profileDiaryData
-  },
-  'Content-Type: application/json'
-);
-
-mock.onGet(`${baseURL}${apiUrl}/cities/`).reply(
-  200,
-  {
-    cities
-  },
-  'Content-Type: application/json'
-);
-
-mock.onGet(`${baseURL}${apiUrl}/afisha/events/`).reply(
-  200,
-  {
-    calendarPageData
-  },
-  'Content-Type: application/json'
-);
-
-mock.onPost(`${baseURL}${apiUrl}/token/`).reply(
-  200,
-  {
-    token
-  },
-  'Content-Type: application/json'
-);
-
-//! functions (new fetch-es)
-// в режиме мок-адаптера все работает как (response) => response.data
-// когда будет нормальный сервер надо будет делать как:
-// (response) => response.json() + проверять статусы response и тогда возвращать res.json()
-export const getMainPageData = () => axios.get(`${baseURL}${apiUrl}/main/`);
-export const getProfileDiaryData = () => axios.get(`${baseURL}${apiUrl}/profile/diary/`).then((response) => response.data);
-export const getCities = () => axios.get(`${baseURL}${apiUrl}/cities/`).then((response) => response.data);
-export const getCalendarPageData = () => axios.get(`${baseURL}${apiUrl}/afisha/events/`).then((response) => response.data);
-
-// login
-//! один из методов auth
-export const authorize = (loginData) => axios.post(`${baseURL}${apiUrl}/token/`, loginData).then((response) => response.data);
-
-// фиксация заголовков, Headers -> Authorization
-// ИЗ ТЗ: После авторизации во все запросы добавляем заголовок
-// --header 'Authorization: Bearer "значение access"'
-export const setAuth = (accessToken) => {
-  axios.defaults.headers.get.Authorization = `Bearer ${accessToken}`;
-  axios.defaults.headers.post.Authorization = `Bearer ${accessToken}`;
-  console.log(axios.defaults.headers);
-};
-
-// жесткая фиксация заголовков 'Content-Type' на весь сеанс
 axios.defaults.headers.get['Content-Type'] = 'application/json';
 axios.defaults.headers.post['Content-Type'] = 'application/json';
+export default class Api {
+  // главная страница
+  static getMainPageData() {
+    return axios
+      .get(`${baseURL}${apiUrl}/main/`)
+      .then((response) => response.data);
+  }
 
-// очистка при логауте
-export const clearAuth = () => {
-  axios.defaults.headers.get.Authorization = '';
-  axios.defaults.headers.post.Authorization = '';
-  console.log(axios.defaults.headers);
-};
+  // города
+  static getCities() {
+    return axios
+      .get(`${baseURL}${apiUrl}/cities/`)
+      .then((response) => response.data);
+  }
 
-// букд / не букд + обновление мест
-// фетч-аксиос
-export const updateEventFetch = (eventData) => axios.patch(`${baseURL}${apiUrl}/afisha/event-participants/`, eventData).then((response) => response.data);
-// вспомогательная функция замоканного ответа
-const updateEventMock = (calendarCard) => {
-  const calendarData = JSON.parse(calendarCard.data);
-  calendarData.booked = !calendarData.booked;
-  calendarData.seats = calendarData.booked ? calendarData.seats - 1 : calendarData.seats + 1;
-  return [200, calendarData];
-};
-// замоканный ответ
-mock.onPatch(`${baseURL}${apiUrl}/afisha/event-participants/`).reply(updateEventMock);
+  // страница календаря (ивенты)
+  static getCalendarPageData() {
+    return axios
+      .get(`${baseURL}${apiUrl}/afisha/events/`)
+      .then((response) => response.data);
+  }
 
-// получение пользователя с сервера
-//! один из методов auth
-export const getUserData = () => axios.get(`${baseURL}${apiUrl}/profile/`).then((response) => response.data);
-// замоканный ответ
-mock.onGet(`${baseURL}${apiUrl}/profile/`).reply(
-  200,
-  {
-    userData
-  },
-  'Content-Type: application/json'
-);
+  // работа с ивентами (карточки)
+  static updateEvent(eventData) {
+    return axios
+      .patch(`${baseURL}${apiUrl}/afisha/event-participants/`, eventData)
+      .then((response) => response.data);
+  }
+
+  // работа с юзером
+  static updateUserInfo(userData) {
+    return axios
+      .patch(`${baseURL}${apiUrl}/profile/`, userData)
+      .then((response) => response.data);
+  }
+
+  // работа со страницей ЛК
+  static getProfileDiaryData() {
+    return axios
+      .get(`${baseURL}${apiUrl}/profile/diary/`)
+      .then((response) => response.data);
+  }
+
+  //! все ответы будут потом переписаны на res.ok ? res.json() : reject()
+  // _handleResult(response) {
+  //   response.ok ? (response.json()) : Promise.reject(`Ошибка ${res.status} ${res.statusText}`)
+  // }
+}
