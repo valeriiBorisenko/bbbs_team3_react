4bc1aa57d9bd41fa104b9a866ca34c5b77b939b7
Alyona Irisova <elaineirr.910@gmail.com>
2021-06-06 18:14:56 +0400

    feat: добавить первую половину формы

--- src/components/ui/FormRecomendation/FormRecomendation.js
+++ src/components/ui/FormRecomendation/FormRecomendation.js
@@ -1,42 +1,123 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import './FormRecomendation.scss';
+import { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { useForm } from 'react-hook-form';
 import Input from '../Input/Input';
+import Button from '../Button/Button';
 
-function FormRecomendation() {
+function FormRecomendation({ isOpen }) {
+  const {
+    register, handleSubmit, formState: { errors }, reset
+  } = useForm();
+
+  const onFormSubmit = (values) => {
+    console.log(values);
+  };
+
+  useEffect(() => {
+    reset({
+      title: '',
+      website: '',
+      address: ''
+    });
+  }, [isOpen]);
+
   return (
-    <form noValidate>
+    <form name="formRecomendation" className="form-recom" onSubmit={handleSubmit(onFormSubmit)}>
+      <div className="form-recom__input-container">
+        <Input
+          sectionClass="form-recom__input"
+          type="text"
+          name="title"
+          placeholder="Название*"
+          register={register}
+          required
+          error={errors?.title}
+          errorMessage="Название*"
+        />
+        <Input
+          sectionClass="form-recom__input"
+          type="text"
+          name="website"
+          placeholder="Сайт"
+          register={register}
+        />
+      </div>
+
       <Input
-        sectionClass="popup__input"
+        sectionClass="form-recom__input"
         type="text"
-        name="place"
-        placeholder="Название*"
-        // register={register}
-        required
-        // error={errors?.login}
-        errorMessage="Название*"
-      />
-      <Input
-        sectionClass="popup__input"
-        type="text"
-        name="website"
-        placeholder="Сайт"
-        // register={register}
-        // required
-        // error={errors?.login}
-        errorMessage="Сайт"
-      />
-      <Input
-        sectionClass="popup__input"
-        type="text"
-        name="adress"
+        name="address"
         placeholder="Адрес*"
-        // register={register}
+        register={register}
         required
-        // error={errors?.login}
+        error={errors?.address}
         errorMessage="Адрес*"
       />
 
-      {/* девочка / мальчик / пол */}
+      <div className="form-recom__input-container">
+        <label className="form-recom__label" htmlFor="formRecommendationBoy">
+          <input
+            id="formRecommendationBoy"
+            className="form-recom__input-radio"
+            type="radio"
+            name="sex"
+            {...register('sex', { required: 'Мальчик' })}
+            value="Мальчик"
+          />
+          <span className={`form-recom__pseudo-radio ${errors?.sex ? 'form-recom__pseudo-radio_error' : ''}`}>Мальчик</span>
+        </label>
+        <label className="form-recom__label" htmlFor="formRecommendationGirl">
+          <input
+            id="formRecommendationGirl"
+            className="form-recom__input-radio"
+            type="radio"
+            name="sex"
+            {...register('sex', { required: 'Девочка' })}
+            value="Девочка"
+          />
+          <span className={`form-recom__pseudo-radio ${errors?.sex ? 'form-recom__pseudo-radio_error' : ''}`}>Девочка</span>
+        </label>
+        <Input
+          sectionClass="form-recom__input"
+          type="number"
+          name="age"
+          placeholder="Возраст*"
+          register={register}
+          required
+          error={errors?.age}
+          errorMessage="Возраст*"
+        />
+      </div>
+
+      <Input
+        sectionClass="form-recom__input form-recom__input_textarea"
+        type="text"
+        name="address"
+        placeholder="Комментарий* Поделитесь впечатлениями о проведенном времени"
+        register={register}
+        required
+        error={errors?.address}
+        errorMessage="Комментарий* Поделитесь впечатлениями о проведенном времени"
+        isTextarea
+      />
+
+      <Button
+        title="Отправить"
+        color="blue"
+        isSubmittable
+      />
     </form>
   );
 }
+
+FormRecomendation.propTypes = {
+  isOpen: PropTypes.bool
+};
+
+FormRecomendation.defaultProps = {
+  isOpen: false
+};
 
 export default FormRecomendation;
--- src/components/ui/WhereToGoPreview/WhereToGoPreview.js
+++ src/components/ui/WhereToGoPreview/WhereToGoPreview.js
@@ -12,16 +12,14 @@ function WhereToGoPreview() {
 
   return (
     <section className="recommendation recommendation_place_page">
-      <div className="recommendation__texts">
-        <p className="section-title recommendation__text">
-          Если вы были в интересном месте и хотите порекомендовать его другим&nbsp;наставникам
-          –&nbsp;
-          <a href="/" className="recommendation__text-link" onClick={handleRecommendPlace}>
-            заполните&nbsp;форму
-          </a>
-          , и мы добавим вашу&nbsp;рекомендацию.
-        </p>
-      </div>
+      <p className="section-title recommendation__text">
+        Если вы были в интересном месте и хотите порекомендовать его другим&nbsp;наставникам
+        –&nbsp;
+        <a href="/" className="recommendation__text-link" onClick={handleRecommendPlace}>
+          заполните&nbsp;форму
+        </a>
+        , и мы добавим вашу&nbsp;рекомендацию.
+      </p>
       {/* вызов формы */}
       <FormRecomendation />
     </section>
