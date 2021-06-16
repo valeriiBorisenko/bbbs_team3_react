053d1a83a216ea571922ad42d8fe6450f7acbd28
Nikita Volkhonskiy <literature198@gmail.com>
2021-06-16 22:58:38 +0300

    feat: создать новый общий контейнер с lottie-анимациями для страниц 404 и заглушки Календаря

---
+++ src/components/AnimatedPageContainer/index.js
@@ -0,0 +1,12 @@
+import { useSmoothScrollOnWindow } from '../../utils/custom-hooks';
+import './AnimatedPageContainer.scss';
+import BasePage from '../../layout/BasePage';
+import animation404 from '../../assets/animation/Illustration_404.json';
+import TitleH2 from '../utils/TitleH2/TitleH2';
+
+export {
+  useSmoothScrollOnWindow,
+  BasePage,
+  animation404,
+  TitleH2
+};
