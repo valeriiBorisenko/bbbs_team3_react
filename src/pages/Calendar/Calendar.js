5cdae2edab705dc4f9cc83e67d17de27a38b00bd
Nikita Volkhonskiy <literature198@gmail.com>
2021-06-16 15:38:55 +0300

    feat: сделать фильтрацию календаря

--- src/pages/Calendar/Calendar.js
+++ src/pages/Calendar/Calendar.js
@@ -22,19 +22,15 @@ function Calendar({
   dataCalendar,
   onOpenLoginPopup
 }) {
-  console.log('запуск функции Calendar');
   useSmoothScrollOnWindow({ top: 0 });
   const currentUser = useContext(CurrentUserContext);
 
-  function eventSignUpHandler(cardData) {
+  const eventSignUpHandler = (cardData) => {
     onEventSignUpClick(cardData, cardData.booked);
-  }
+  };
 
   // весь список доступных фильтров
   const [filters, setFilters] = useState([]);
-
-  // текущие выбранные настройки фильтрации
-  const [activeFilter, setActiveFilter] = useState(null);
 
   // флаг использования фильтров
   const [isFiltersUsed, setIsFiltersUsed] = useState(false);
@@ -42,38 +38,28 @@ function Calendar({
   // мутабельный массив для применения фильтров (отсортированный)
   const [sortedArray, setSortedArray] = useState([]);
 
-  // массив отфильтрованных ивентов под нажатый фильтр, готовый к отрисовке
+  // массив отфильтрованных ивентов с даты, не мутировать!
   const [filteredCardData, setFilteredCardData] = useState([]);
 
-  const handleFilterClick = (filter, inputName, isChecked) => {
-    console.group('handleFilterClick');
-    console.log('filter', filter);
-    console.log('inputName', inputName);
-    console.log('isChecked', isChecked);
-    console.groupEnd();
-    setIsFiltersUsed(true);
-    setActiveFilter(filter);
+  // хэндлер клика по фильтру МЕСЯЦ
+  const handleFilterClick = (inputName, isChecked) => {
     changeRadioTagState(setFilters, { inputName, isChecked });
+    setIsFiltersUsed(true);
   };
 
   //! первый useEffect, установка отсортированного массива
   useEffect(() => {
-    console.group('запуск первого useEffect');
-    console.log('dataCalendar', dataCalendar);
-    console.groupEnd();
     const arrayOfSortedEvent = [...dataCalendar].sort((a, b) => {
       const date1 = new Date(a.startAt);
       const date2 = new Date(b.startAt);
       return date1 - date2;
     });
-
+    setFilteredCardData(arrayOfSortedEvent);
     setSortedArray(arrayOfSortedEvent);
   }, [dataCalendar]);
-  // console.log(sortedArray);
 
   //! второй useEffect, сбор списка фильтров
   useEffect(() => {
-    console.group('запуск второго useEffect');
     //* ШАГ 2 из массива отсоритрованных ивентов делаем массив объектов {месяц, год} на каждый ивент
     const arrayOfDatesWithEvents = sortedArray.map((someEvent) => {
       // взяли дату ивента, переделали в дату-объект
@@ -95,40 +81,39 @@ function Calendar({
     const finallArrayOfTagsData = arrayOfUniqueDates.map((filter) => {
       const name = changeCaseOfFirstLetter(months[filter.month]);
       return {
-        filter,
+        filter: JSON.stringify(filter),
         name,
         isActive: false
       };
     });
-    console.log('finalTagsData', finallArrayOfTagsData);
-    console.groupEnd();
 
     setFilters(finallArrayOfTagsData);
-  }, [sortedArray]);
+  }, [filteredCardData]);
 
-  //! третий useEffect, фильтрация при нажатии
-  useEffect(() => {
-    console.group('третий useEffect');
-    if (activeFilter) {
-      const { year, month } = activeFilter;
-
-      const min = new Date(year, month);
-      const max = new Date(year, month + 1);
-      const filteredArrayOfEvents = sortedArray.filter((eventItem) => {
-        const date = new Date(eventItem.startAt);
-        return date >= min && date <= max;
-      });
-      console.log('filteredArrayOfEvents', filteredArrayOfEvents);
-
-      setFilteredCardData(filteredArrayOfEvents);
-      // setIsFiltersUsed(false);
-      setActiveFilter(null);
+  // функция-фильтратор
+  const handleFiltration = () => {
+    if (isFiltersUsed) {
+      const activeFilter = filters.find((filter) => filter.isActive);
+      if (!activeFilter) {
+        setSortedArray(filteredCardData);
+      } else {
+        const { month, year } = JSON.parse(activeFilter.filter);
+        const min = new Date(year, month);
+        const max = new Date(year, month + 1);
+        const filteredArrayOfEvents = filteredCardData.filter((eventItem) => {
+          const date = new Date(eventItem.startAt);
+          return date >= min && date <= max;
+        });
+        setSortedArray(filteredArrayOfEvents);
+      }
     }
-    console.groupEnd();
-    // activeFilter
-  }, [activeFilter]);
+  };
 
-  const whatDataToRender = isFiltersUsed ? filteredCardData : sortedArray;
+  //! третий useEffect (запуск фильтрации)
+  useEffect(() => {
+    handleFiltration();
+    setIsFiltersUsed(false);
+  }, [isFiltersUsed]);
 
   return (
     <BasePage>
@@ -150,7 +135,7 @@ function Calendar({
             )}
 
             <div className="calendar-page__grid">
-              {whatDataToRender.map((data) => (
+              {sortedArray.map((data) => (
                 <CardCalendar
                   key={data.id}
                   cardData={data}
@@ -183,124 +168,3 @@ Calendar.defaultProps = {
 };
 
 export default Calendar;
-
-//! все что было раньше
-//! ШАГ 1 = сортируем все ивенты по хронологии
-// const arrayOfSortedEvents = [...dataCalendar].sort((a, b) => {
-//   const date1 = new Date(a.startAt);
-//   const date2 = new Date(b.startAt);
-//   return date1 - date2;
-// });
-// console.log('dataCalendar', dataCalendar);
-// console.log('arrayOfSortedEvents', arrayOfSortedEvents);
-// const arrayOfSortedEvents = dataCalendar.sort((a, b) => {
-//   const date1 = new Date(a.startAt);
-//   const date2 = new Date(b.startAt);
-//   return date1 - date2;
-// });
-
-//! фильтрация ивентов под нажатый фильтр
-// useEffect(() => {
-//   if (filterSettings) {
-//     const { year, monthNumber } = filterSettings;
-//     const min = new Date(year, monthNumber);
-//     const max = new Date(year, monthNumber + 1);
-//     const filteredArrayOfEvents = arrayOfSortedEvents.filter((eventItem) => {
-//       const date = new Date(eventItem.startAt);
-
-//       return date >= min && date <= max;
-//     });
-//     setFilteredCardData(filteredArrayOfEvents);
-//   }
-
-// //   setIsFiltersUsed(isChecked);
-// // }, [isChecked, arrayOfSortedEvents, filterSettings]);
-// }, [arrayOfSortedEvents, filterSettings]);
-
-// function handleCheckboxChange(evt, filters) {
-//   const { target } = evt;
-//   setIsChecked(target.checked);
-//   setFilterSettings(filters);
-// }
-
-// function handleCheckboxClick(evt, value) {
-//   const { target } = evt;
-//   const values = value.split('-');
-//   if (filterSettings) {
-//     if (target.checked
-//         && Number(values[0]) === filterSettings.monthNumber
-//         && Number(values[1]) === filterSettings.year
-//     ) {
-//       target.checked = false;
-//       setIsChecked(target.checked);
-//       setFilterSettings(null);
-//     }
-//   }
-// }
-//! ШАГ 2 = из массива ивентов делаем массив вида [месяц, год] на каждый ивент
-// const arrayOfDatesWithEvents = arrayOfSortedEvents.map((someEvent) => {
-//   // взяли дату ивента, переделали в дату-объект
-//   const dateObj = new Date(someEvent.startAt);
-//   const month = dateObj.getMonth();
-//   const year = dateObj.getFullYear();
-//   // в массив вернулся НОМЕР месяца и ГОД
-//   return { month, year };
-// });
-// console.log('arrayOfDatesWithEvents', arrayOfDatesWithEvents);
-
-//! ШАГ 3 = выкидываем из массива arrayOfDatesWithEvents повторы
-// const arrayOfUniqueDates = Object.values(
-//   arrayOfDatesWithEvents.reduce((res, item) => ({
-//     ...res,
-//     [item.join('-')]: item
-//   }), {})
-// );
-// const arrayOfUniqueDates = arrayOfDatesWithEvents
-//   .filter((item, index, self) => self
-//     .findIndex((t) => (t.month === item.month && t.year === item.year)) === index);
-// console.log('arrayOfUniqueDatesNEW', arrayOfUniqueDates);
-
-// const final = arrayOfUniqueDates.map((filter) => {
-//   const name = changeCaseOfFirstLetter(months[filter.month]);
-//   return {
-//     filter,
-//     name,
-//     isActive: false
-//   };
-// });
-// console.log('final', final);
-
-// setFilters(final);
-// setFilters(() => );
-// console.log(filters);
-
-// const tags = arrayOfUniqueDates.map((date) => {
-
-// });
-//! ШАГ 4 = на основании хронологического массива без дубликатов генерируем кнопки
-// function tagsButtonsToRender() {
-//   return arrayOfUniqueDates.map((date) => {
-//     const tagTitle = months[date[0]];
-//     const monthNumber = months.indexOf(months[date[0]]);
-//     const year = date[1];
-//     const filters = { year, monthNumber };
-
-//     return (
-//       <li className="tags__list-item" key={`${monthNumber}-${year}`}>
-//         <PseudoButtonTag
-//           type="radio"
-//           name="months"
-//           value={`${monthNumber}-${year}`}
-//           title={tagTitle}
-//           filters={filters}
-//           onChange={handleCheckboxChange}
-//           onClick={handleCheckboxClick}
-//         />
-//       </li>
-//     );
-//   });
-// }
-//* ШАГ 3 выкидываем из массива arrayOfDatesWithEvent повторы
-// const arrayOfUniqueDates = arrayOfDatesWithEvents
-//   .filter((item, index, self) => self
-//     .findIndex((t) => (t.month === item.month && t.year === item.year)) === index);
