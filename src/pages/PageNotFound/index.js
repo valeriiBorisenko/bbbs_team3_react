d4c00fc3ab80708efcbbc78e18b5c733d139ad16
Nikita Volkhonskiy <literature198@gmail.com>
2021-06-17 13:43:56 +0300

    feat: удалить ненужные импорты

--- src/pages/PageNotFound/index.js
+++ src/pages/PageNotFound/index.js
@@ -1,11 +1,9 @@
 import { useSmoothScrollOnWindow } from '../../utils/custom-hooks';
 import BasePage from '../../layout/BasePage';
-// import animation404 from '../../assets/animation/Illustration_404.json';
 import AnimatedPageContainer from '../../components/AnimatedPageContainer/AnimatedPageContainer';
 
 export {
   useSmoothScrollOnWindow,
   BasePage,
-  // animation404,
   AnimatedPageContainer
 };
