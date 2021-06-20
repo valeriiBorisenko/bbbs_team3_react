4f15f6fc4da8f33c0610a6ae8024686d760ebbc6
Alyona Irisova <elaineirr.910@gmail.com>
2021-06-20 20:20:48 +0400

    fix: фикс страницы ЛК

---
+++ src/pages/Profile/index.js
@@ -0,0 +1,8 @@
+import BasePage from '../../layout/BasePage';
+import AccountEventCard from '../../components/Cards/AccountEventCard/AccountEventCard';
+import TitleH2 from '../../components/utils/TitleH2/TitleH2';
+import AccountForm from '../../components/AccountForm/AccountForm';
+import AccountDiary from '../../components/AccountDiary/AccountDiary';
+import PopupDeleteDiary from '../../components/Popups/PopupDeleteDiary/PopupDeleteDiary';
+
+export { BasePage, AccountEventCard, TitleH2, AccountForm, AccountDiary, PopupDeleteDiary };
