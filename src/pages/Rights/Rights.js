d41dd418b3052775d7f16694902adf86e373fc8f
Artyom Lodochnikov <lodkin55@yandex.ru>
2021-06-24 09:32:00 +0500

    update: Дописал первую версию page Rights

--- src/pages/Rights/Rights.js
+++ src/pages/Rights/Rights.js
@@ -1,80 +1,36 @@
-/* eslint-disable no-unused-vars */
+import React, { useState, useEffect } from 'react';
+import { Helmet } from 'react-helmet-async';
 import './Rights.scss';
 
-import React, { useState, useEffect, useContext } from 'react';
-import { Helmet } from 'react-helmet-async';
-
-import CurrentUserContext from '../../contexts/CurrentUserContext';
-import { BasePage, TitleH1 } from './index';
-import { useScrollToTop } from '../../hooks/index';
-import { ALL_CATEGORIES } from '../../config/constants';
+import { ALL_CATEGORIES, FIGURES, COLORS } from '../../config/constants';
 import {
   renderFilterTags,
   handleCheckboxBehavior,
   selectOneTag,
   deselectOneTag,
 } from '../../utils/filter-tags';
-import Loader from '../../components/utils/Loader/Loader';
+import { useScrollToTop } from '../../hooks/index';
 
-// Временный хардкод данных рубрик без информации при клике
-const articlesData = [
-  {
-    category: 'Пенсии',
-    title: 'Пенсионное обеспечение для детей-сирот',
-    type: 'Рубрика',
-    id: 1,
-  },
-  {
-    category: 'Образование',
-    title: 'Пенсионное обеспечение для детей-сирот',
-    type: 'Рубрика',
-    id: 2,
-  },
-  {
-    category: 'Транспорт',
-    title: 'Пенсионное обеспечение для детей-сирот',
-    type: 'Рубрика',
-    id: 3,
-  },
-  {
-    category: 'Трудоустройство',
-    title: 'Пенсионное обеспечение для детей-сирот',
-    type: 'Рубрика',
-    id: 4,
-  },
-  {
-    category: 'Пенсии',
-    title: 'Пенсионное обеспечение для детей-сирот',
-    type: 'Рубрика',
-    id: 5,
-  },
-  {
-    category: 'Транспорт',
-    title: 'Пенсионное обеспечение для детей-сирот',
-    type: 'Рубрика',
-    id: 6,
-  },
-  {
-    category: 'Транспорт',
-    title: 'Пенсионное обеспечение для детей-сирот',
-    type: 'Рубрика',
-    id: 7,
-  },
-];
+// json data
+import rightsData from '../../utils/server-responses/rights.json';
+import rightsTags from '../../utils/server-responses/rights-tags.json';
+
+import { BasePage, Loader, TitleH1, Paginate, CardRights } from './index';
 
 const Rights = () => {
   useScrollToTop();
 
-  const currentUser = useContext(CurrentUserContext);
-
+  // Стейты для пагинации
   const [pageSize, setPageSize] = useState(16);
   const [pageCount, setPageCount] = useState(0);
   const [pageNumber, setPageNumber] = useState(0);
 
+  // Стейты с данными Статьи, Теги, Отфильтрованные Статьи
   const [pageData, setPageData] = useState([]);
   const [categories, setCategories] = useState([]);
   const [filteredArticles, setFilteredArticles] = useState([]);
 
+  // Загрузка данных
   const [isLoading, setIsLoading] = useState(false);
   // флаг применения фильтров
   const [isFiltersUsed, setIsFiltersUsed] = useState(false);
@@ -102,7 +58,7 @@ const Rights = () => {
     // КАТЕГОРИИ
     if (activeCategories.length > 0) {
       const filterByCategory = pageData.filter((article) =>
-        activeCategories.includes(article.category)
+        article.tags.some((tag) => activeCategories.includes(tag.category))
       );
 
       setFilteredArticles(filterByCategory);
@@ -111,13 +67,22 @@ const Rights = () => {
     }
   };
 
+  // запуск фильтрации
+  useEffect(() => {
+    handleFiltration();
+    setIsFiltersUsed(false);
+  }, [isFiltersUsed]);
+
   // Временно адаптирован под хардкод компонента и отсутствие АПИ
   useEffect(() => {
-    setIsLoading(false);
-    setPageData(articlesData);
-    setFilteredArticles(articlesData);
+    setIsLoading(true);
+    // const offset = pageSize * pageNumber;
+    // Здесь будет АПИ запросы тегов и карточек
+    setPageData(rightsData.results);
+    setFilteredArticles(rightsData.results);
+    setPageCount(Math.ceil(rightsData.count / pageSize));
 
-    const categoriesArr = articlesData.map((article) => article.category);
+    const categoriesArr = rightsTags.map((tag) => tag.category);
     const set = new Set(categoriesArr);
     const uniqueCategories = Array.from(set).map((item) => ({
       filter: item,
@@ -129,42 +94,34 @@ const Rights = () => {
       { filter: ALL_CATEGORIES, name: ALL_CATEGORIES, isActive: true },
       ...uniqueCategories,
     ]);
+
+    setIsLoading(false);
+  }, [pageSize, pageNumber]);
+
+  // Юз эффект для пагинации
+  useEffect(() => {
+    const smallQuery = window.matchMedia('(max-width: 1439px)');
+    const largeQuery = window.matchMedia('(max-width: 1919px)');
+
+    const listener = () => {
+      if (smallQuery.matches) {
+        setPageSize(4);
+      } else if (largeQuery.matches) {
+        setPageSize(9);
+      } else {
+        setPageSize(16);
+      }
+    };
+    listener();
+
+    smallQuery.addEventListener('change', listener);
+    largeQuery.addEventListener('change', listener);
+
+    return () => {
+      smallQuery.removeEventListener('change', listener);
+      largeQuery.removeEventListener('change', listener);
+    };
   }, []);
-
-  // ---- Вероятно хуки для работы с отрисовкой страницы??
-  // useEffect(() => {
-  //   setIsLoading(true);
-  //   const offset = pageSize * pageNumber;
-  //   Api.ЗАРОС({ limit: pageSize, offset }).then(({ results, count }) => {
-  //     setCatalogPageData(results);
-  //     setPageCount(Math.ceil(count / pageSize));
-  //     setIsLoading(false);
-  //   });
-  // }, [pageSize, pageNumber]);
-
-  // useEffect(() => {
-  //   const smallQuery = window.matchMedia('(max-width: 1439px)');
-  //   const largeQuery = window.matchMedia('(max-width: 1919px)');
-
-  //   const listener = () => {
-  //     if (smallQuery.matches) {
-  //       setPageSize(4);
-  //     } else if (largeQuery.matches) {
-  //       setPageSize(9);
-  //     } else {
-  //       setPageSize(16);
-  //     }
-  //   };
-  //   listener();
-
-  //   smallQuery.addEventListener('change', listener);
-  //   largeQuery.addEventListener('change', listener);
-
-  //   return () => {
-  //     smallQuery.removeEventListener('change', listener);
-  //     largeQuery.removeEventListener('change', listener);
-  //   };
-  // }, []);
 
   return (
     <BasePage>
@@ -177,19 +134,45 @@ const Rights = () => {
       </Helmet>
       <section className="lead page__section fade-in">
         <TitleH1 title="Права детей" />
-        {isLoading ? (
-          <Loader isNested />
-        ) : (
-          <>
-            <div className="tags">
-              <ul className="tags__list">
-                {renderFilterTags(categories, 'checkbox', changeCategory)}
-              </ul>
-            </div>
-            <div />
-          </>
+        {!isLoading && (
+          <div className="tags">
+            <ul className="tags__list">
+              {renderFilterTags(categories, 'checkbox', changeCategory)}
+            </ul>
+          </div>
         )}
       </section>
+      {isLoading ? (
+        <Loader isNested />
+      ) : (
+        <>
+          <section className="rights page__section">
+            {/* !!Нужно решить проблему с полосками они жестко захаркожены!! */}
+            <div className="rights__line" />
+            <div className="rights__line" />
+            <div className="rights__line" />
+            {filteredArticles.map((item, i) => (
+              <CardRights
+                key={item.id}
+                title={item.title}
+                tags={item.tags}
+                // временные значение shape, color поскольку данные будут прихдить от БЭКА?
+                shape={FIGURES[i % FIGURES.length]}
+                color={COLORS[i % COLORS.length]}
+              />
+            ))}
+          </section>
+
+          <section className="rights-pagination page__section">
+            <Paginate
+              sectionClass="cards-section__pagination"
+              pageCount={pageCount}
+              value={pageNumber}
+              onChange={setPageNumber}
+            />
+          </section>
+        </>
+      )}
     </BasePage>
   );
 };
--- src/pages/Rights/index.js
+++ src/pages/Rights/index.js
@@ -1,7 +1,7 @@
 import BasePage from '../../layout/BasePage';
 import Loader from '../../components/utils/Loader/Loader';
 import TitleH1 from '../../components/utils/TitleH1/TitleH1';
-import CardFigure from '../../components/Cards/CardFigure/CardFigure';
+import Paginate from '../../components/utils/Paginate/Paginate';
+import CardRights from '../../components/Cards/CardRights/CardRights';
 
-// eslint-disable-next-line object-curly-newline
-export { BasePage, Loader, TitleH1, CardFigure };
+export { BasePage, Loader, TitleH1, Paginate, CardRights };
