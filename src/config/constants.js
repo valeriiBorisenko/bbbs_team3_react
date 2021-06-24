e983434558f3d387e7d7971afddc692d6ade9dbf
Alyona Irisova <elaineirr.910@gmail.com>
2021-06-24 21:27:43 +0400

    feat: добавить .bmp в список разрешенных файлов

--- src/config/constants.js
+++ src/config/constants.js
@@ -52,4 +52,4 @@ export const FIGURES = [
 
 export const ALL_CATEGORIES = 'Все';
 
-export const regExpImages = /^[\W\w\S\s]+\.(jpe?g|png|webp|jfif)$/i;
+export const regExpImages = /^[\W\w\S\s]+\.(jpe?g|png|webp|jfif|bmp)$/i;
