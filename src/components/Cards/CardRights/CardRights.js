d366413e5ce2519be57de4697173a499e4d6ddb0
Artyom Lodochnikov <lodkin55@yandex.ru>
2021-06-24 09:33:44 +0500

    feat: Компонент для статьи CardRights

---
+++ src/components/Cards/CardRights/CardRights.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import './CardRights.scss';
+
+import PropTypes from 'prop-types';
+
+import { CardFigure, Rubric } from './index';
+
+function CardRights({ title, tags, shape, color }) {
+  return (
+    <div className="catalog-card card-pagination card-pagination_type_shapes">
+      <a href="/" className="rights__link">
+        <CardFigure
+          sectionClass="card-catalog_type_shaped"
+          shape={shape}
+          title={title}
+          color={color}
+        >
+          {tags.map((tag) => (
+            <Rubric
+              key={tag.id}
+              sectionClass="rights__rubric"
+              title={tag.name}
+            />
+          ))}
+        </CardFigure>
+      </a>
+    </div>
+  );
+}
+
+CardRights.propTypes = {
+  title: PropTypes.string,
+  shape: PropTypes.string,
+  tags: PropTypes.arrayOf(Array),
+  color: PropTypes.string,
+};
+
+CardRights.defaultProps = {
+  title: '',
+  shape: 'square',
+  tags: [],
+  color: '',
+};
+
+export default CardRights;
---
+++ src/components/Cards/CardRights/index.js
@@ -0,0 +1,4 @@
+import CardFigure from '../CardFigure/CardFigure';
+import Rubric from '../../utils/Rubric/Rubric';
+
+export { CardFigure, Rubric };
