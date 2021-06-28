b00da615d1a0a093048193e6cfc9934817d4c3c7
Artyom Lodochnikov <lodkin55@yandex.ru>
2021-06-28 17:19:36 +0500

    update: Исправлены стили отображения категорий

--- src/components/Cards/CardRights/CardRights.js
+++ src/components/Cards/CardRights/CardRights.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './CardRights.scss';
 
 import PropTypes from 'prop-types';
@@ -6,22 +6,30 @@ import PropTypes from 'prop-types';
 import { CardFigure, Rubric } from './index';
 
 function CardRights({ title, tags, shape, color }) {
+  const [uniqueCategories, setUniqueCategories] = useState([]);
+
+  const randNum = () => Math.random().toString(36).substr(2, 9);
+
+  // Убираем одинаковые категории, отрисовываем только уникальные
+  useEffect(() => {
+    const categoriesArr = tags.map((tag) => tag.category);
+    const unique = Array.from(new Set(categoriesArr));
+    setUniqueCategories(unique);
+  }, [tags]);
+
   return (
     <div className="rights__card">
       <a href="/" className="rights__link">
-        <CardFigure
-          sectionClass="card-catalog_type_shaped"
-          shape={shape}
-          title={title}
-          color={color}
-        >
-          {tags.map((tag) => (
-            <Rubric
-              key={tag.id}
-              sectionClass="rights__rubric"
-              title={tag.category}
-            />
-          ))}
+        <CardFigure shape={shape} title={title} color={color}>
+          <div className="rights__block">
+            {uniqueCategories.map((item) => (
+              <Rubric
+                key={randNum()}
+                sectionClass="rights__rubric"
+                title={item}
+              />
+            ))}
+          </div>
         </CardFigure>
       </a>
     </div>
