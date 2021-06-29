e362f70653f58d4ef124285dc53160172fb9fd24
Artyom Lodochnikov <lodkin55@yandex.ru>
2021-06-29 09:58:31 +0500

    fix: Убрать лишнюю логику фильтрации

--- src/components/Cards/CardRights/CardRights.js
+++ src/components/Cards/CardRights/CardRights.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './CardRights.scss';
 
 import PropTypes from 'prop-types';
@@ -6,27 +6,16 @@ import PropTypes from 'prop-types';
 import { CardFigure, Rubric } from './index';
 
 function CardRights({ title, tags, shape, color }) {
-  const [uniqueCategories, setUniqueCategories] = useState([]);
-
-  const randNum = () => Math.random().toString(36).substr(2, 9);
-
-  // Убираем одинаковые категории, отрисовываем только уникальные
-  useEffect(() => {
-    const categoriesArr = tags.map((tag) => tag.category);
-    const unique = Array.from(new Set(categoriesArr));
-    setUniqueCategories(unique);
-  }, [tags]);
-
   return (
     <div className="rights__card">
       <a href="/" className="rights__link">
         <CardFigure shape={shape} title={title} color={color}>
           <div className="rights__block">
-            {uniqueCategories.map((item) => (
+            {tags.map((tag) => (
               <Rubric
-                key={randNum()}
+                key={tag.id}
                 sectionClass="rights__rubric"
-                title={item}
+                title={tag.name}
               />
             ))}
           </div>
