e5bdecf359712f1172ede33750cfad2368647642
Nikita Volkhonskiy <literature198@gmail.com>
2021-07-07 22:20:02 +0300

    refactor: переработать импорты для страницы Читать и смотреть

--- src/pages/ReadAndWatch/index.js
+++ src/pages/ReadAndWatch/index.js
@@ -1,26 +1,19 @@
 import BasePage from '../../layout/BasePage';
 import Loader from '../../components/utils/Loader/Loader';
-import TitleH3 from '../../components/utils/TitleH3/TitleH3';
-// карточка справочник
-import CardCatalog from '../../components/Cards/CardCatalog/CardCatalog';
-//! карточка видео (нету)
-// карточка статьи
-import CardArticle from '../../components/Cards/CardArticle/CardArticle';
-// карточка фильмы
-import CardFilm from '../../components/Cards/CardFilm/CardFilm';
-//! карточка книги (нету)
-
-// import CardPlace from '../../components/Cards/';
-import CardsSectionWithLines from '../../components/Cards/CardsSectionWithLines/CardsSectionWithLines';
 import AnimatedPageContainer from '../../components/AnimatedPageContainer/AnimatedPageContainer';
+
+// карточки
+import CardCatalog from '../../components/Cards/CardCatalog/CardCatalog';
+import CardArticle from '../../components/Cards/CardArticle/CardArticle';
+import CardFilm from '../../components/Cards/CardFilm/CardFilm';
+import CardBook from '../../components/Cards/CardBook/CardBook';
 
 export {
   BasePage,
   Loader,
-  TitleH3,
   CardCatalog,
   CardArticle,
   CardFilm,
-  CardsSectionWithLines,
+  CardBook,
   AnimatedPageContainer,
 };
