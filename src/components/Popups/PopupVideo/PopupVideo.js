8ae0b03e3c94921d591e38ac393d488bd3b5e8b3
Artyom Lodochnikov <lodkin55@yandex.ru>
2021-07-08 18:18:24 +0500

    feat: Popup Video

---
+++ src/components/Popups/PopupVideo/PopupVideo.js
@@ -0,0 +1,58 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import './PopupVideo.scss';
+import PropTypes from 'prop-types';
+
+import Popup from '../Popup/Popup';
+import { TitleH2, Caption } from '../../utils/index';
+
+function youtubeParser(url) {
+  const regExp =
+    /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
+  const match = url.match(regExp);
+  return match && match[7].length === 11 ? match[7] : false;
+}
+
+function getUrlThumbnail(link) {
+  const id = youtubeParser(link);
+  const imagePreview = `http://img.youtube.com/vi/${id}/maxresdefault.jpg`;
+  return imagePreview;
+}
+
+const PopupVideo = ({ data, isOpen, onClose }) => {
+  const { title, info, link } = data;
+  return (
+    <Popup
+      type="video"
+      typeContainer="video"
+      isOpen={isOpen}
+      onClose={onClose}
+      withoutCloseButton
+    >
+      <iframe
+        title={title}
+        className="popup__video-iframe"
+        src="https://www.youtube.com/embed/U4QCyP_klLk"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      />
+
+      <TitleH2 sectionClass="popup__video-title" title={title} />
+      <Caption sectionClass="popup__video-caption" title={info} />
+    </Popup>
+  );
+};
+
+PopupVideo.propTypes = {
+  data: PropTypes.objectOf(PropTypes.any),
+  isOpen: PropTypes.bool,
+  onClose: PropTypes.func,
+};
+
+PopupVideo.defaultProps = {
+  data: {},
+  isOpen: false,
+  onClose: () => {},
+};
+
+export default PopupVideo;
