b9b03ca0f1a072b27b27d9532c864e8e1b3629a2
Artyom Lodochnikov <lodkin55@yandex.ru>
2021-07-09 11:56:41 +0500

    update: Дописал логику попапа

--- src/components/Popups/PopupVideo/PopupVideo.js
+++ src/components/Popups/PopupVideo/PopupVideo.js
@@ -1,44 +1,69 @@
-/* eslint-disable no-unused-vars */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './PopupVideo.scss';
 import PropTypes from 'prop-types';
 
 import Popup from '../Popup/Popup';
 import { TitleH2, Caption } from '../../utils/index';
-
-function parserLinkYoutube(link) {
-  const regExp =
-    /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
-  const match = link.match(regExp);
-  return match && match[7].length === 11 ? match[7] : false;
-}
-
-function getUrlThumbnail(link) {
-  const id = parserLinkYoutube(link);
-  const imagePreview = `http://img.youtube.com/vi/${id}/maxresdefault.jpg`;
-  return imagePreview;
-}
+import parserLinkYoutube from '../../../utils/parser-link-youtube';
+import Loader from '../../utils/Loader/Loader';
 
 const PopupVideo = ({ data, isOpen, onClose }) => {
-  const { title, info, link } = data;
+  const { id, title, info, link } = data;
+
+  const [videoSrc, setVideoSrc] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  // Обнуляем юзстейт с ссылкой при закрытии
+  const close = () => {
+    onClose();
+    setVideoSrc(null);
+  };
+
+  // без асинк/евейт мелькает предыдущее видео при открытии нового
+  const getSrcFrame = async () => {
+    const src = await parserLinkYoutube(link);
+    setVideoSrc(src);
+
+    // Искуственная задержка что бы увидеть лоадер
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2 * 1000);
+  };
+
+  // При открытии нового попапас с видео
+  // запускаем загруку и парсер
+  useEffect(() => {
+    setIsLoading(true);
+    getSrcFrame();
+  }, [id]);
+
   return (
     <Popup
       type="video"
       typeContainer="video"
       isOpen={isOpen}
-      onClose={onClose}
+      onClose={close}
       withoutCloseButton
     >
-      <iframe
-        title={title}
-        className="popup__video-iframe"
-        src="https://www.youtube.com/embed/U4QCyP_klLk"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-      />
+      {isLoading ? (
+        <Loader isNested />
+      ) : (
+        <>
+          <iframe
+            title="youTubePlayer"
+            id="playeryt"
+            className="popup__video-iframe"
+            src={`${videoSrc}`}
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+            seamless
+          />
 
-      <TitleH2 sectionClass="popup__video-title" title={title} />
-      <Caption sectionClass="popup__video-caption" title={info} />
+          <TitleH2 sectionClass="popup__video-title" title={title} />
+          <Caption sectionClass="popup__video-caption" title={info} />
+        </>
+      )}
     </Popup>
   );
 };
