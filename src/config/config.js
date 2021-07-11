307e71a2a33b135bb38423dff8d239cd49b5e5e0
valeriiBorisenko <tjvell@yandex.ru>
2021-07-12 02:54:04 +0300

    feat: правка api сервера

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,4 @@
-export const baseURL = 'http://localhost:80';
+export const baseURL = 'http://178.154.192.53';
 export const apiUrl = '/api/v1';
 export const adminUrl = 'http://127.0.0.1';
 export const staticImageUrl = `${adminUrl}/media`;
