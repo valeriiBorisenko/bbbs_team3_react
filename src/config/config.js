670b1a39f288899a738b5aae9f2e6d49f3540e8b
Nikita Volkhonskiy <literature198@gmail.com>
2021-07-12 15:01:21 +0300

    refactor: изменить адрес сайта в config,js, теперь это наш постоянный АПИ

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
-export const baseURL = 'http://localhost:80';
-// export const baseURL = 'http://178.154.192.53:80'; нужно при создании общего билда на яндекс облаке, подставляй вместе baseURL
+// export const baseURL = 'http://localhost:80';
+export const baseURL = 'http://178.154.192.53:80';
 export const apiUrl = '/api/v1';
 export const adminUrl = 'http://127.0.0.1';
 export const staticImageUrl = `${adminUrl}/media`;
