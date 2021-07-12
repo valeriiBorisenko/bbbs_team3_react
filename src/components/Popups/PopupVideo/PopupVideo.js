dee98d4cdcd193eda32e6f56728006d7193f1b60
Nikita Volkhonskiy <literature198@gmail.com>
2021-07-12 12:07:20 +0300

    fix: сделать рефакторинг попапа видео, убрать лишние стейты, добавить лоадер

--- src/components/Popups/PopupVideo/PopupVideo.js
+++ src/components/Popups/PopupVideo/PopupVideo.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import './PopupVideo.scss';
 import PropTypes from 'prop-types';
 import Popup from '../Popup/Popup';
@@ -13,82 +13,17 @@ import { localStChosenVideo } from '../../../config/constants';
 
 const PopupVideo = ({ isOpen, onClose }) => {
   console.log('PopupVideo COMPONENT');
-
-  //! если убирать лоадер вообще, то можно оставить embedSrc и загрузку даты из стораджа
-  // даже не понадобится еффекты и стейты
   const data = getLocalStorageData(localStChosenVideo);
+  const embedSrc = parserLinkYoutube(data?.link);
 
-  // const embedSrc = parserLinkYoutube(data?.link);
-
-  // const [data, setData] = useState(null);
-  // setData(getLocalStorageData(localStChosenVideo))
   console.log('data', data);
-  // const { id, title, info, link } = data;
 
-  // const [videoSrc, setVideoSrc] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
-
-  // без асинк/евейт мелькает предыдущее видео при открытии нового
-  const getSrcFrame = () => {
-    const src = parserLinkYoutube(data?.link);
-    // setVideoSrc(src);
-
-    // Искуственная задержка что бы увидеть лоадер
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 800);
-    return src;
-  };
+  const [iframeIsLoading, setIframeIsLoading] = useState(true);
 
   const onCloseHandler = () => {
-    // setVideoSrc(null);
-    // setData(null);
     removeLocalStorageData(localStChosenVideo);
     onClose();
   };
-
-  // При открытии нового попапа с видео
-  // запускаем загруку и парсер
-
-  useEffect(() => {
-    console.log('ПОПАП ВИДЕО ЮЗЕФФЕКТ');
-    if (isOpen) {
-      setIsLoading(true);
-      // setData(getLocalStorageData(localStChosenVideo));
-      // getSrcFrame();
-    }
-  }, [isOpen]);
-
-  function renderPopup() {
-    console.log('renderPopup FUNC');
-    if (isLoading) {
-      console.log('isLoading IF');
-      return <Loader isNested />;
-    }
-
-    if (data) {
-      console.log('data && videoSrc IF ');
-      return (
-        <>
-          <iframe
-            title="youTubePlayer"
-            id="playeryt"
-            className="popup__video-iframe"
-            // src={videoSrc}
-            src={getSrcFrame()}
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-            seamless
-          />
-          <TitleH2 sectionClass="popup__video-title" title={data?.title} />
-          <Caption sectionClass="popup__video-caption" title={data?.info} />
-        </>
-      );
-    }
-
-    return null;
-  }
 
   return (
     <Popup
@@ -98,21 +33,22 @@ const PopupVideo = ({ isOpen, onClose }) => {
       onClose={onCloseHandler}
       withoutCloseButton
     >
-      {/* <iframe
-        title="youTubePlayer"
-        id="playeryt"
-        className="popup__video-iframe"
-        src={embedSrc}
-        // src={getSrcFrame()}
-        frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-        seamless
-      />
-      <TitleH2 sectionClass="popup__video-title" title={data?.title} />
-      <Caption sectionClass="popup__video-caption" title={data?.info} /> */}
-
-      {renderPopup()}
+      {iframeIsLoading ? <Loader isNested /> : null}
+      <>
+        <iframe
+          title="youTubePlayer"
+          id="playeryt"
+          onLoad={() => setIframeIsLoading(false)}
+          className="popup__video-iframe"
+          src={embedSrc}
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+          seamless
+        />
+        <TitleH2 sectionClass="popup__video-title" title={data?.title} />
+        <Caption sectionClass="popup__video-caption" title={data?.info} />
+      </>
     </Popup>
   );
 };
