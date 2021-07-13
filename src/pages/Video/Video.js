14758513a8a6e6282cc32e246a2b5712cd2e97f4
Nikita Volkhonskiy <literature198@gmail.com>
2021-07-13 11:09:56 +0300

    feat: сделать открытие попапа Видео при редиректе на старницу видео с главной страницы

--- src/pages/Video/Video.js
+++ src/pages/Video/Video.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import './Video.scss';
 import videoPageTexts from '../../locales/video-page-RU';
 import {
@@ -11,7 +11,11 @@ import {
   AnimatedPageContainer,
   TagsList,
 } from './index';
-import { ALL_CATEGORIES, DELAY_DEBOUNCE } from '../../config/constants';
+import {
+  ALL_CATEGORIES,
+  DELAY_DEBOUNCE,
+  localStChosenVideo,
+} from '../../config/constants';
 import { useScrollToTop, useDebounce } from '../../hooks/index';
 import {
   handleCheckboxBehavior,
@@ -20,6 +24,8 @@ import {
 } from '../../utils/filter-tags';
 import { getVideoPageTags, getVideoPageData } from '../../api/video-page';
 import { changeCaseOfFirstLetter } from '../../utils/utils';
+import { getLocalStorageData } from '../../hooks/useLocalStorage';
+import { PopupsContext } from '../../contexts/index';
 
 const PAGE_SIZE_PAGINATE = {
   small: 4,
@@ -29,6 +35,9 @@ const PAGE_SIZE_PAGINATE = {
 
 const Video = () => {
   useScrollToTop();
+  const { openPopupVideo } = useContext(PopupsContext);
+  const chosenVideoOnRedirectFromMainPage =
+    getLocalStorageData(localStChosenVideo);
 
   const { headTitle, headDescription, title, textStubNoData } = videoPageTexts;
 
@@ -308,6 +317,8 @@ const Video = () => {
         <TitleH1 title={title} sectionClass="video__title" />
         {categories?.length > 1 && !isLoadingPage && renderTagsContainer()}
       </section>
+      {/* Открытие попапа Видео при переходе с Главной страницы */}
+      {chosenVideoOnRedirectFromMainPage && openPopupVideo()}
 
       {renderMainContent()}
     </BasePage>
