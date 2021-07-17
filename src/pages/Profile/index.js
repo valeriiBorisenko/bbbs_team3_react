b38974896aa834f15e52c2507ddb197c2d49baed
Alyona Irisova <elaineirr.910@gmail.com>
2021-07-17 16:48:56 +0400

    fix: поправить импорты

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -3,8 +3,12 @@ import ProfileEventCard from '../../components/Cards/ProfileEventCard/ProfileEve
 import ProfileForm from '../../components/ProfileForm/ProfileForm';
 import ProfileDiary from '../../components/ProfileDiary/ProfileDiary';
 import PopupDeleteDiary from '../../components/Popups/PopupDeleteDiary/PopupDeleteDiary';
-import ScrollableContainer from '../../components/utils/ScrollableContainer/ScrollableContainer';
-import { TitleH2, ButtonRound, Loader } from '../../components/utils/index';
+import {
+  TitleH2,
+  ButtonRound,
+  Loader,
+  ScrollableContainer,
+} from '../../components/utils/index';
 
 export {
   BasePage,
