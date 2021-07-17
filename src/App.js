86f5e6539fe7d077141d9aab1a6696483827f218
Alyona Irisova <elaineirr.910@gmail.com>
2021-07-17 23:00:45 +0400

    fix: исправить функцию закрытия попапа логина в хуке useAuth

--- src/App.js
+++ src/App.js
@@ -143,6 +143,7 @@ function App() {
             <PopupsContext.Provider
               value={{
                 closeAllPopups,
+                closePopupLogin,
                 openPopupConfirmation,
                 openPopupSuccessfully,
                 openPopupAboutEvent,
