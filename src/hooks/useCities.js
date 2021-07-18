575015ab8a1bab791aa559396f0067f08fa0e9f1
Alyona Irisova <elaineirr.910@gmail.com>
2021-07-18 18:27:06 +0400

    fix: убрать обработку из хука useCities

--- src/hooks/useCities.js
+++ src/hooks/useCities.js
@@ -1,14 +1,8 @@
-import { useEffect, useState, useContext } from 'react';
+import { useEffect, useState } from 'react';
 import getCities from '../api/cities';
-import { PopupsContext, ErrorsContext } from '../contexts/index';
-import { DEFAULT_CITY, ERROR_MESSAGES } from '../config/constants';
+import { DEFAULT_CITY } from '../config/constants';
 
 const useCities = () => {
-  const { generalErrorMessage } = ERROR_MESSAGES;
-  // не деструктурируется, на момент чтения undefined
-  const popups = useContext(PopupsContext);
-  const errors = useContext(ErrorsContext);
-
   const [cities, setCities] = useState(null);
   const [defaultCity, setDefaultCity] = useState({});
 
@@ -19,13 +13,7 @@ const useCities = () => {
         const mainCity = citiesData.find((city) => city?.name === DEFAULT_CITY);
         setDefaultCity(mainCity);
       })
-      .catch(() => {
-        errors.setError({
-          title: generalErrorMessage.title,
-          button: generalErrorMessage.button,
-        });
-        popups.openPopupError();
-      });
+      .catch(console.error);
   }, []);
 
   return { cities, defaultCity };
