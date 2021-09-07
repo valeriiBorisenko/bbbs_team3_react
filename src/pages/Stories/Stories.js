29e26889281bf75fed513d600f2673a9d194505f
Alyona Irisova <elaineirr.910@gmail.com>
2021-09-07 19:13:00 +0400

    feat: добавить базовую разметку страницы Stories

--- src/pages/Stories/Stories.js
+++ src/pages/Stories/Stories.js
@@ -1,11 +1,63 @@
-import { BasePage, AnimatedPageContainer } from './index';
+import { useEffect, useState } from 'react';
+import { BasePage, Caption, TagsList, TitleH1, TitleH2 } from './index';
 import storiesPageTexts from '../../locales/stories-page-RU';
+import { staticImageUrl } from '../../config/config';
+import { getStoriesPageTags, getStoryById } from '../../api/stories-page';
+import { formatDate, formatMonthsGenitiveCase } from '../../utils/utils';
+import './Stories.scss';
 
-const { headTitle, headDescription } = storiesPageTexts;
+const { headTitle, headDescription, title, subtitle } = storiesPageTexts;
+
 function Stories() {
+  const [storiesTags, setStoriesTags] = useState([]);
+  const [currentStory, setCurrentStory] = useState({});
+
+  console.log(storiesTags);
+  console.log(currentStory);
+
+  useEffect(() => {
+    getStoriesPageTags()
+      .then(({ count, results }) => {
+        if (count > 0) {
+          getStoryById(results[0].id).then(setCurrentStory).catch(console.log);
+        }
+        const storiesTagsData = results?.map((tag) => ({
+          filter: tag?.id,
+          name: tag?.pair,
+          isActive: false,
+        }));
+
+        setStoriesTags(storiesTagsData);
+      })
+      .catch(console.log);
+  }, []);
+
+  const pairTitle = storiesTags.find((tag) => tag.filter === currentStory?.id);
+  const togetherSince = formatDate(currentStory?.togetherSince);
+
   return (
     <BasePage headTitle={headTitle} headDescription={headDescription}>
-      <AnimatedPageContainer titleText="Данная страница пока находится в разработке. Приносим свои извинения!" />
+      <TitleH1 title={title} sectionClass="stories__title" />
+      <p className="stories__subtitle">{subtitle}</p>
+      <TagsList
+        filterList={storiesTags}
+        name="stories"
+        handleClick={() => {}}
+        sectionClass="stories__tags"
+      />
+      <img
+        className="stories__main-photo"
+        src={`${staticImageUrl}/${currentStory?.image}`}
+        alt={currentStory?.title}
+      />
+      <TitleH2 title={pairTitle?.name} sectionClass="stories__pair-title" />
+      <Caption
+        title={`Вместе с ${formatMonthsGenitiveCase(
+          togetherSince?.monthName
+        )} ${togetherSince?.year} года`}
+        sectionClass="stories__caption"
+      />
+      <p className="stories__subtitle">{currentStory?.description}</p>
     </BasePage>
   );
 }
--- src/pages/Stories/index.js
+++ src/pages/Stories/index.js
@@ -1,4 +1,6 @@
 import BasePage from '../../layout/BasePage';
 import AnimatedPageContainer from '../../components/AnimatedPageContainer/AnimatedPageContainer';
+import TagsList from '../../components/TagsList/TagsList';
+import { Caption, TitleH1, TitleH2 } from '../../components/utils';
 
-export { BasePage, AnimatedPageContainer };
+export { BasePage, Caption, TitleH1, TitleH2, TagsList, AnimatedPageContainer };
