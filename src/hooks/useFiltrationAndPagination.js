25da767fb74ce5e14802f496ab86b2b3b2a05dde
Alyona Irisova <elaineirr.910@gmail.com>
2021-09-26 16:13:46 +0400

    refactor: черновик хука useFiltrationaAndPagination

--- src/hooks/useFiltrationAndPagination.js
+++ src/hooks/useFiltrationAndPagination.js
@@ -1,3 +1,152 @@
-const useFiltrationAndPagination = () => {};
+import { useContext, useEffect, useState } from 'react';
+import { ErrorsContext, PopupsContext } from '../contexts';
+import {
+  ALL_CATEGORIES,
+  DELAY_DEBOUNCE,
+  ERROR_MESSAGES,
+} from '../config/constants';
+import useDebounce from './useDebounce';
+import { changeCaseOfFirstLetter } from '../utils/utils';
+import {
+  deselectOneTag,
+  handleCheckboxBehavior,
+  selectOneTag,
+} from '../utils/filter-tags';
+
+// apiFilterName - { filterName: name }
+
+const useFiltrationAndPagination = ({
+  apiGetDataCallback,
+  apiGetFiltersCallback,
+  apiFilterName,
+  pageSize,
+  pageErrorSetter,
+}) => {
+  const { setError } = useContext(ErrorsContext);
+  const { openPopupError } = useContext(PopupsContext);
+  // данные
+  const [dataToRender, setDataToRender] = useState([]);
+  const [filters, setFilters] = useState([]);
+  // лоадеры, флаги
+  const [isFiltersUsed, setIsFiltersUsed] = useState(false);
+  const [isPaginationUsed, setIsPaginationUsed] = useState(false);
+  const [isPageLoading, setIsPageLoading] = useState(true);
+  // пагинация
+  const [totalPages, setTotalPages] = useState(0);
+  const [pageIndex, setPageIndex] = useState(0);
+
+  const getActiveFilters = () => {
+    if (filters) {
+      return filters
+        .filter((f) => f.isActive && f.filter !== ALL_CATEGORIES)
+        .map((f) => f.filter)
+        .join(',');
+    }
+    return null;
+  };
+
+  // работа с API
+  const getFilters = () => {
+    apiGetFiltersCallback()
+      .then((tags) => {
+        const filtersArray = tags.map((tag) => ({
+          filter: tag?.slug.toLowerCase(),
+          name: changeCaseOfFirstLetter(tag?.name),
+          isActive: false,
+        }));
+
+        setFilters([
+          { filter: ALL_CATEGORIES, name: ALL_CATEGORIES, isActive: true },
+          ...filtersArray,
+        ]);
+      })
+      .catch(() => pageErrorSetter(true));
+  };
+
+  const getData = () => {
+    const tags = { [apiFilterName]: getActiveFilters() };
+    const offset = isFiltersUsed ? 0 : pageSize * pageIndex;
+
+    apiGetDataCallback({ limit: pageSize, offset, ...tags })
+      .then(({ results, count }) => {
+        setTotalPages(Math.ceil(count / pageSize));
+        setDataToRender(results);
+      })
+      .catch(() => {
+        if (isFiltersUsed) {
+          setError(ERROR_MESSAGES.filterErrorMessage);
+          openPopupError();
+        } else {
+          pageErrorSetter(true);
+        }
+      })
+      .finally(() => {
+        setIsPageLoading(false);
+        setIsPaginationUsed(false);
+        setIsFiltersUsed(false);
+      });
+  };
+
+  // хэндлер клика по фильтру
+  const changeFilter = (inputValue, isChecked) => {
+    if (inputValue === ALL_CATEGORIES) {
+      selectOneTag(setFilters, ALL_CATEGORIES);
+    } else {
+      handleCheckboxBehavior(setFilters, { inputValue, isChecked });
+      deselectOneTag(setFilters, ALL_CATEGORIES);
+    }
+    setIsFiltersUsed(true);
+  };
+
+  // функция-фильтратор с использованием АПИ
+  const handleFiltration = () => {
+    if (filters && isFiltersUsed) {
+      const activeCategories = getActiveFilters();
+
+      if (activeCategories.length === 0) {
+        selectOneTag(setFilters, ALL_CATEGORIES);
+      }
+      getData();
+    }
+  };
+
+  const debounceFiltration = useDebounce(handleFiltration, DELAY_DEBOUNCE);
+  const debouncePaginate = useDebounce(getData, DELAY_DEBOUNCE);
+
+  // Первая отрисовка страницы
+  useEffect(() => {
+    if (isPageLoading && pageSize) {
+      getData();
+      getFilters();
+    }
+  }, [pageSize]);
+
+  // Переход по пагинации, страница уже загружена
+  useEffect(() => {
+    if (!isPageLoading && !isFiltersUsed) {
+      setIsPaginationUsed(true);
+      debouncePaginate();
+    }
+  }, [pageSize, pageIndex]);
+
+  // Фильтры, страница уже загружена
+  useEffect(() => {
+    if (!isPageLoading && isFiltersUsed) {
+      debounceFiltration();
+    }
+  }, [isFiltersUsed]);
+
+  return {
+    dataToRender,
+    filters,
+    isPageLoading,
+    isFiltersUsed,
+    isPaginationUsed,
+    totalPages,
+    pageIndex,
+    setPageIndex,
+    changeFilter,
+  };
+};
 
 export default useFiltrationAndPagination;
--- src/pages/Books/Books.js
+++ src/pages/Books/Books.js
@@ -1,19 +1,8 @@
-import { useContext, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import booksPageTexts from './locales/RU';
-import { ErrorsContext, PopupsContext } from '../../contexts';
-import {
-  ALL_CATEGORIES,
-  DELAY_DEBOUNCE,
-  ERROR_MESSAGES,
-} from '../../config/constants';
-import { useDebounce, usePageWidth } from '../../hooks';
-import { changeCaseOfFirstLetter } from '../../utils/utils';
-import {
-  deselectOneTag,
-  handleCheckboxBehavior,
-  selectOneTag,
-} from '../../utils/filter-tags';
+import { ERROR_MESSAGES } from '../../config/constants';
+import { useFiltrationAndPagination, usePageWidth } from '../../hooks';
 import {
   getBook,
   getBooksPageData,
@@ -45,25 +34,25 @@ const MAX_SCREEN_WIDTH = {
 const { headTitle, headDescription, title, textStubNoData } = booksPageTexts;
 
 function Books() {
-  const { setError } = useContext(ErrorsContext);
-  const { openPopupError } = useContext(PopupsContext);
+  // const { setError } = useContext(ErrorsContext);
+  // const { openPopupError } = useContext(PopupsContext);
 
   const { state } = useLocation();
   const searchBookId = state?.id;
   const [searchedBook, setSearchedBook] = useState({});
 
   // Загрузка данных
-  const [isLoading, setIsLoading] = useState(true);
-  const [isLoadingPaginate, setIsLoadingPaginate] = useState(false);
+  // const [isLoading, setIsLoading] = useState(true);
+  // const [isLoadingPaginate, setIsLoadingPaginate] = useState(false);
   // Стейты с данными Книг, Теги
-  const [booksPageData, setBooksPageData] = useState(null);
-  const [categories, setCategories] = useState(null);
+  // const [booksPageData, setBooksPageData] = useState(null);
+  // const [categories, setCategories] = useState(null);
   // флаг применения фильтров
-  const [isFiltersUsed, setIsFiltersUsed] = useState(false);
+  // const [isFiltersUsed, setIsFiltersUsed] = useState(false);
   // Стейты для пагинации
   const pageSize = usePageWidth(MAX_SCREEN_WIDTH, PAGE_SIZE_PAGINATE);
-  const [pageCount, setPageCount] = useState(0);
-  const [pageNumber, setPageNumber] = useState(0);
+  // const [pageCount, setPageCount] = useState(0);
+  // const [pageNumber, setPageNumber] = useState(0);
   // Стейт ошибки
   const [isPageError, setIsPageError] = useState(false);
   const [isBookPopupOpen, setIsBookPopupOpen] = useState(false);
@@ -76,84 +65,104 @@ function Books() {
     setIsBookPopupOpen(false);
   };
 
-  const getActiveTags = () => {
-    if (categories) {
-      return categories
-        .filter((filter) => filter.isActive && filter.filter !== ALL_CATEGORIES)
-        .map((filter) => filter.filter)
-        .join(',');
-    }
-    return null;
+  const FiltersAndPaginationSettings = {
+    apiGetDataCallback: getBooksPageData,
+    apiGetFiltersCallback: getBooksPageFilter,
+    apiFilterName: 'types',
+    pageSize,
+    pageErrorSetter: setIsPageError,
   };
 
-  const getBooksData = (activeCategories) => {
-    const offset = isFiltersUsed ? 0 : pageSize * pageNumber;
-    const activeTags = activeCategories || getActiveTags();
+  const {
+    dataToRender,
+    filters,
+    isPageLoading,
+    isFiltersUsed,
+    isPaginationUsed,
+    totalPages,
+    pageIndex,
+    setPageIndex,
+    changeFilter,
+  } = useFiltrationAndPagination(FiltersAndPaginationSettings);
 
-    getBooksPageData({
-      limit: pageSize,
-      offset,
-      types: activeTags,
-    })
-      .then(({ results, count }) => {
-        setPageCount(Math.ceil(count / pageSize));
-        return results;
-      })
-      .then((results) => setBooksPageData(results))
-      .catch(() => {
-        if (isFiltersUsed) {
-          setError(ERROR_MESSAGES.filterErrorMessage);
-          openPopupError();
-        } else {
-          setIsPageError(true);
-        }
-      })
-      .finally(() => {
-        setIsLoading(false);
-        setIsLoadingPaginate(false);
-        setIsFiltersUsed(false);
-      });
-  };
+  // const getActiveTags = () => {
+  //   if (categories) {
+  //     return categories
+  //       .filter((filter) => filter.isActive && filter.filter !== ALL_CATEGORIES)
+  //       .map((filter) => filter.filter)
+  //       .join(',');
+  //   }
+  //   return null;
+  // };
 
-  const getBooksFilter = () => {
-    getBooksPageFilter()
-      .then((tags) => {
-        const categoriesArr = tags.map((tag) => ({
-          filter: tag?.slug.toLowerCase(),
-          name: changeCaseOfFirstLetter(tag?.name),
-          isActive: false,
-        }));
+  // const getBooksData = (activeCategories) => {
+  //   const offset = isFiltersUsed ? 0 : pageSize * pageNumber;
+  //   const activeTags = activeCategories || getActiveTags();
+  //
+  //   getBooksPageData({
+  //     limit: pageSize,
+  //     offset,
+  //     types: activeTags,
+  //   })
+  //     .then(({ results, count }) => {
+  //       setPageCount(Math.ceil(count / pageSize));
+  //       return results;
+  //     })
+  //     .then((results) => setBooksPageData(results))
+  //     .catch(() => {
+  //       if (isFiltersUsed) {
+  //         setError(ERROR_MESSAGES.filterErrorMessage);
+  //         openPopupError();
+  //       } else {
+  //         setIsPageError(true);
+  //       }
+  //     })
+  //     .finally(() => {
+  //       setIsLoading(false);
+  //       setIsLoadingPaginate(false);
+  //       setIsFiltersUsed(false);
+  //     });
+  // };
 
-        setCategories([
-          { filter: ALL_CATEGORIES, name: ALL_CATEGORIES, isActive: true },
-          ...categoriesArr,
-        ]);
-      })
-      .catch(() => setIsPageError(true));
-  };
+  // const getBooksFilter = () => {
+  //   getBooksPageFilter()
+  //     .then((tags) => {
+  //       const categoriesArr = tags.map((tag) => ({
+  //         filter: tag?.slug.toLowerCase(),
+  //         name: changeCaseOfFirstLetter(tag?.name),
+  //         isActive: false,
+  //       }));
+  //
+  //       setCategories([
+  //         { filter: ALL_CATEGORIES, name: ALL_CATEGORIES, isActive: true },
+  //         ...categoriesArr,
+  //       ]);
+  //     })
+  //     .catch(() => setIsPageError(true));
+  // };
 
   // хэндлер клика по фильтру КАТЕГОРИЯ
-  const changeCategory = (inputValue, isChecked) => {
-    if (inputValue === ALL_CATEGORIES) {
-      selectOneTag(setCategories, ALL_CATEGORIES);
-    } else {
-      handleCheckboxBehavior(setCategories, { inputValue, isChecked });
-      deselectOneTag(setCategories, ALL_CATEGORIES);
-    }
-    setIsFiltersUsed(true);
-  };
+  // const changeCategory = (inputValue, isChecked) => {
+  //   if (inputValue === ALL_CATEGORIES) {
+  //     selectOneTag(setCategories, ALL_CATEGORIES);
+  //   } else {
+  //     handleCheckboxBehavior(setCategories, { inputValue, isChecked });
+  //     deselectOneTag(setCategories, ALL_CATEGORIES);
+  //   }
+  //   setIsFiltersUsed(true);
+  // };
 
   // функция-фильтратор с использованием АПИ
-  const handleFiltration = () => {
-    if (categories && isFiltersUsed) {
-      const activeCategories = getActiveTags();
-
-      if (activeCategories.length === 0) {
-        selectOneTag(setCategories, ALL_CATEGORIES);
-      }
-      getBooksData(activeCategories);
-    }
-  };
+  // const handleFiltration = () => {
+  //   if (categories && isFiltersUsed) {
+  //     const activeCategories = getActiveTags();
+  //
+  //     if (activeCategories.length === 0) {
+  //       selectOneTag(setCategories, ALL_CATEGORIES);
+  //     }
+  //     getBooksData(activeCategories);
+  //   }
+  // };
 
   // Откртие попапа при переходе из поиска
   useEffect(() => {
@@ -168,29 +177,29 @@ function Books() {
   }, [state]);
 
   /// Фильтрация с делэем
-  const debounceFiltration = useDebounce(handleFiltration, DELAY_DEBOUNCE);
-  const debouncePaginate = useDebounce(getBooksData, DELAY_DEBOUNCE);
-  useEffect(() => {
-    if (isFiltersUsed) {
-      debounceFiltration();
-    }
-  }, [isFiltersUsed]);
+  // const debounceFiltration = useDebounce(handleFiltration, DELAY_DEBOUNCE);
+  // const debouncePaginate = useDebounce(getBooksData, DELAY_DEBOUNCE);
+  // useEffect(() => {
+  //   if (isFiltersUsed) {
+  //     debounceFiltration();
+  //   }
+  // }, [isFiltersUsed]);
 
   // Первая отрисовка страницы + переход по страницам пагинации
-  useEffect(() => {
-    if (isLoading && pageSize) {
-      getBooksData();
-      getBooksFilter();
-    }
-
-    if (!isLoading && !isFiltersUsed) {
-      setIsLoadingPaginate(true);
-      debouncePaginate();
-    }
-  }, [pageSize, pageNumber]);
+  // useEffect(() => {
+  //   if (isLoading && pageSize) {
+  //     getBooksData();
+  //     getBooksFilter();
+  //   }
+  //
+  //   if (!isLoading && !isFiltersUsed) {
+  //     setIsLoadingPaginate(true);
+  //     debouncePaginate();
+  //   }
+  // }, [pageSize, pageNumber]);
 
   // глобальный лоадер
-  if (isLoading) {
+  if (isPageLoading) {
     return <Loader isCentered />;
   }
 
@@ -222,11 +231,11 @@ function Books() {
         <TitleH1 title={title} sectionClass="books__title" />
 
         {/* рендер фильтров */}
-        {categories?.length > 1 && (
+        {filters?.length > 1 && (
           <TagsList
-            filterList={categories}
+            filterList={filters}
             name="tag"
-            handleClick={changeCategory}
+            handleClick={changeFilter}
           />
         )}
 
@@ -237,26 +246,26 @@ function Books() {
   }
 
   function renderBooksContainer() {
-    if (!booksPageData && !isLoading) {
+    if (!dataToRender && !isPageLoading) {
       return renderAnimatedContainer();
     }
     return (
       <>
-        {isLoadingPaginate ? (
+        {isPaginationUsed ? (
           <Loader isPaginate />
         ) : (
           <ul className="books__cards cards-grid cards-grid_content_small-cards fade-in">
-            {booksPageData.map((books) => (
+            {dataToRender.map((books) => (
               <CardBook key={books.id} data={books} sectionClass="scale-in" />
             ))}
           </ul>
         )}
-        {pageCount > 1 && (
+        {totalPages > 1 && (
           <Paginate
             sectionClass="cards-section__pagination"
-            pageCount={pageCount}
-            value={pageNumber}
-            onChange={setPageNumber}
+            pageCount={totalPages}
+            value={pageIndex}
+            onChange={setPageIndex}
           />
         )}
       </>
