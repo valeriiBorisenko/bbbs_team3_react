7ede5954e096c5dc71f19cd58c35860f771ec43e
Alyona Irisova <elaineirr.910@gmail.com>
2021-09-28 01:17:17 +0400

    refactor: рефактор формирования возрастных фильтров

--- src/utils/age-filters.js
+++ src/utils/age-filters.js
@@ -2,27 +2,10 @@ import placesPageTexts from '../pages/Places/locales/RU';
 
 const { ageFilterNames } = placesPageTexts;
 
-const ageFiltersArray = [
-  {
-    filter: ageFilterNames[0].filter,
-    name: ageFilterNames[0].name,
-    isActive: false,
-  },
-  {
-    filter: ageFilterNames[1].filter,
-    name: ageFilterNames[1].name,
-    isActive: false,
-  },
-  {
-    filter: ageFilterNames[2].filter,
-    name: ageFilterNames[2].name,
-    isActive: false,
-  },
-  {
-    filter: ageFilterNames[3].filter,
-    name: ageFilterNames[3].name,
-    isActive: false,
-  },
-];
+const ageFiltersArray = ageFilterNames.map((ageFilter) => ({
+  filter: ageFilter.filter,
+  name: ageFilter.name,
+  isActive: false,
+}));
 
 export default ageFiltersArray;
