c1cf5d1d014d7176267d0f0151568ad82a360b82
Alyona Irisova <elaineirr.910@gmail.com>
2021-09-28 19:03:08 +0400

    fix: вернуть delay debounce к 300ms

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
-const isDev = true;
+const isDevevelopment = true;
 const developmentIP = 'http://178.154.192.53';
-const prodIP = 'http://195.24.66.127';
-export const baseURL = isDev ? developmentIP : prodIP;
+const productionIP = 'http://195.24.66.127';
+export const baseURL = isDevevelopment ? developmentIP : productionIP;
 export const apiUrl = '/api/v1';
-export const adminUrl = isDev ? developmentIP : prodIP;
+export const adminUrl = isDevevelopment ? developmentIP : productionIP;
 export const staticImageUrl = `${adminUrl}/media`;
