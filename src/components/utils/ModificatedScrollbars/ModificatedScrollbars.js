c80c9b97bd24d81ddd63cb54a440859c6d58e2ea
Alyona Irisova <elaineirr.910@gmail.com>
2021-10-02 18:17:19 +0400

    refactor: добавить библиотеку react-custom-scrollbars, переработать скроллбары

--- src/components/utils/ModificatedScrollbars/ModificatedScrollbars.js
+++ src/components/utils/ModificatedScrollbars/ModificatedScrollbars.js
@@ -1,29 +1,23 @@
-import { Scrollbars } from 'react-custom-scrollbars';
-import { useRef } from 'react';
+import CustomScroll from 'react-custom-scroll';
 import PropTypes from 'prop-types';
 
-function ModificatedScrollbars({ horizontalScrollClass, children }) {
-  const ref = useRef();
-
+function ModificatedScrollbars({ children }) {
   return (
-    <Scrollbars
-      ref={ref}
-      renderTrackHorizontal={() => <div style={{ opacity: 'none' }} />}
-      renderThumbVertical={() => <div className={horizontalScrollClass} />}
+    <CustomScroll
+    // renderTrackHorizontal={() => <div style={{ opacity: 'none' }} />}
+    // renderThumbVertical={() => <div className={horizontalScrollClass} />}
     >
       {children}
-    </Scrollbars>
+    </CustomScroll>
   );
 }
 
 ModificatedScrollbars.propTypes = {
   children: PropTypes.node,
-  horizontalScrollClass: PropTypes.string,
 };
 
 ModificatedScrollbars.defaultProps = {
   children: null,
-  horizontalScrollClass: '',
 };
 
 export default ModificatedScrollbars;
