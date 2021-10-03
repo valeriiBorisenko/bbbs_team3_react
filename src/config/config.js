74367596b9bce71f59178d8bc23fc3c4ac0eb3b6
Alyona Irisova <elaineirr.910@gmail.com>
2021-10-03 21:08:09 +0400

    fix: фикс стиля большой карточки книги

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,4 @@
-export const baseURL = 'http://178.154.192.53';
+export const baseURL = 'http://195.24.66.127';
 export const apiUrl = '/api/v1';
-export const adminUrl = 'http://178.154.192.53';
+export const adminUrl = 'http://195.24.66.127';
 export const staticImageUrl = `${adminUrl}/media`;
